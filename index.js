32c2c6521521415eb417abc7c4798d693feef3df
micho19 <damiangiannico@gmail.com>
2021-03-26 10:28:44 -0300

    Entregar

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const {
   removeById,
 } = require('./crudOp');
 
-createFile('clients/clients.json');
-// readFile('clients/clients.json');
-appendUser('clients/clients.json');
-// removeById('clients/clients.json', 'f820a90b-7659-4320-b97d-d94f4243600c');
-// removeLast('clients/clients.json');
+createFile('clients.json');
+// readFile('clients.json');
+appendUser('clients.json');
+// removeById('clients.json', 'f820a90b-7659-4320-b97d-d94f4243600c');
+// removeLast('clients.json');
